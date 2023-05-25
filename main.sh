f9f9c3e083520dc71425fab015c4179be60ce039
root <root@DESKTOP-9NN1EVF.localdomain>
2023-05-25 17:58:51 +0200

    Implementation de CRON, avec la souvergarde en zip et l'envoie a l'horaire donnee sur le server

--- main.sh
+++ main.sh
@@ -6,15 +6,14 @@ MAIL_PWD=$3
 MAIL_LOGINSED=$(echo "$MAIL_LOGIN" | sed 's/@/%40/g')
 
 
-
 mkdir /home/shared
 
 ### TELECHARGEMENT D'ECLIPSE ET MISE EN SERVICE
+#apt get install wget
 # wget -O eclipse.tar.gz <https://www.eclipse.org/downloads/download.php?file=/oomph/epp/2023-03/R/eclipse-inst-jre-win64.exe>
+#sudo tar -xf eclipse.tar.gz -C /opt/
 
-sudo tar -xf eclipse.tar.gz -C /opt/
-
-# Set permissions
+# Set permissions#
 sudo chown -R root:root /opt/eclipse
 sudo chmod -R +r /opt/eclipse
 
@@ -25,9 +24,10 @@ ssh -i /home/isen/.ssh/id_rsa asoque25@10.30.48.100 "mkdir saves"
 
 ### FIN DE LA MISE EN FONCTION D'ECLIPSE
 
-### INSTALLATION DE SWAKS POUR L'ENVOIE DU MAIL
-apt-get install swaks
 
+file="tamponCron.txt"
+touch $file
+crontab -u isen -l > $file
 
 while read -r line; do	
 	IFS=';' read -r nom prenom mail motdepasse <<< "$line"	
@@ -43,9 +43,10 @@ while read -r line; do
 	sudo chown "$login"  /home/shared/"$login" ###Seul l'utilisateur dont son nom est le fichier peut modifier le dossier 
 	sudo chmod 755 /home/shared/"$login"
 	
-	tar -czf /home/"$login"/save_"$login".tgz /home/"$login"/a_sauver
-	scp -i /home/isen/.ssh/id_rsa /home/"$login"/save_"$login".tgz asoque25@10.30.48.100:~/saves
-
+	#tar -czf /home/"$login"/save_"$login".tgz /home/"$login"/a_sauver
+	#scp -i /home/isen/.ssh/id_rsa /home/"$login"/save_"$login".tgz asoque25@10.30.48.100:~/saves
+	line="0 23 * * 1-5 isen tar -czf /home/"$login"/save_"$login".tgz /home/"$login"/a_sauver;scp -i /home/isen/.ssh/id_rsa /home/"$login"/save_"$login".tgz asoque25@10.30.48.100:~/saves;rm  /home/"$login"/save_"$login".tgz"
+	echo "$line" >> $file
 
 	# Commande pour l'envoie du mail depuis le server
 	TO="$mail"
@@ -55,25 +56,26 @@ Voici vos informations de connexion :
 Nom d'utilisateur : $login   
 Mot de passe :$motdepasse  
 Veuillez changer votre mot de passe lors de votre premiere connexion."
-#	
-        ssh -n -i /home/isen/.ssh/id_rsa asoque25@10.30.48.100 "mail --subject \"$SUBJECT \" --exec \"set sendmail=smtp://$MAIL_LOGINSED:$MAIL_PWD@smtp.$MAIL_SERVER\" --append \"From:$MAIL_LOGIN\" antonsoquet@gmail.com <<<\"$BODY\" "
+
+        ssh -n -i /home/isen/.ssh/id_rsa asoque25@10.30.48.100 "mail --subject \"$SUBJECT \" --exec \"set sendmail=smtp://$MAIL_LOGINSED:$MAIL_PWD@smtp.$MAIL_SERVER\" --append \"From:$MAIL_LOGIN\" \"$mail\" <<<\"$BODY\" "
 
 
 
 done < <(tail -n +2 accounts.csv)
 
-### MISE EN PLACE DE LA SOUVEGARDE DES FICHIERS DES UTILISATEURS SUR LE SERVEUR
 
-# Compress the contents of a_sauver directory to .tgz format
-# nom = "Agnes"  
-#  tar -czf /home/Agnes/save_"$nom".tgz /home/Agnes/a_sa
-    # Copy the compressed file to the remote machine's /home/saves directory
-   # scp /home/saves/save_"$nom".tgz asoque25@10.30.48.100:/home/saves/
-
+crontab -u isen $file
+rm "$file"
 ### FIN DE LA SOUVEGARDE DES FICHIERS UTILISATEURS 
 
+
+
+
+
+
+
 #INSTALLATION DE ECLIPSE
-#apt install wgetJ_aimeRENNES&ISEN
+#apt install wget
 #wget https://www.eclipse.org/downloads/download.php?file=/technology/epp/downloads/release/2021-09/R/eclipse-java-2021-09-R-linux-gtk-x86_64.tar.gz -O eclipse.tar.gz
 #tar -xf eclipse.tar.gz
 #mv eclipse /home
